dffc57785eca53e8f8b6a5dd0f50ccd352082f47
Adrian <adrmac@gmail.com>
2025-07-30 17:22:04 +0000

    fixed typescript

--- ui/src/components/CandidateList/VisualizationsStack.tsx
+++ ui/src/components/CandidateList/VisualizationsStack.tsx
@@ -2,17 +2,19 @@ import { Box, Stack, Theme, useMediaQuery } from "@mui/material";
 import React from "react";
 
 import ReportsBarChart from "@/components/CandidateList/ReportsBarChart";
+import { useData } from "@/context/DataContext";
 
 import CandidateListFilters from "./CandidateListFilters";
 import { CandidatesResults } from "./CandidatesResults";
 
 export const VisualizationsStack = () => {
   const mdDown = useMediaQuery((theme: Theme) => theme.breakpoints.down("md"));
+  const { sortedCandidates } = useData();
   return (
     <Stack>
       <CandidateListFilters />
       <Box sx={{ paddingTop: "1.5rem", overflow: mdDown ? "auto" : "initial" }}>
-        <CandidatesResults viewType="chart" />
+        <CandidatesResults viewType="chart" candidates={sortedCandidates} />
         <Box sx={{ paddingTop: "1.5rem" }}></Box>
         <ReportsBarChart />
       </Box>
